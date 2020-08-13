d70911fd1aa9e4a63d65c0efd50262a320cacdb4
suriyaprakash482selvam <69462955+suriyaprakash482selvam@users.noreply.github.com>
2020-08-14 03:59:12 +1400

    add  initial cv website files

---
+++ hyjaj.js
@@ -0,0 +1,98 @@
+document.querySelector(".a").addEventListener("click", handCli1);
+function handCli1() {
+    
+	var audio = new Audio("Music/EM1.wav");
+	audio.play();
+}
+document.querySelector(".b").addEventListener("click", handCli2);
+function handCli2() {
+    
+	var audio = new Audio("Music/EM2.wav");
+	audio.play();
+}
+document.querySelector(".c").addEventListener("click", handCli3);
+function handCli3() {
+    
+	var audio = new Audio("Music/EM3.wav");
+	audio.play();
+}
+document.querySelector(".d").addEventListener("click", handCli4);
+function handCli4() {
+    
+	var audio = new Audio("Music/EM4.wav");
+	audio.play();
+}
+document.querySelector(".e").addEventListener("click", handCli5);
+function handCli5() {
+    
+	var audio = new Audio("Music/EM5.wav");
+	audio.play();
+}
+document.querySelector(".f").addEventListener("click", handCli6);
+function handCli6() {
+    
+	var audio = new Audio("Music/EM6.wav");
+	audio.play();
+}
+document.querySelector(".g").addEventListener("click", handCli7);
+function handCli7() {
+    
+	var audio = new Audio("Music/EM7.wav");
+	audio.play();
+}
+document.querySelector(".h").addEventListener("click", handCli8);
+function handCli8() {
+    
+	var audio = new Audio("Music/EM8.wav");
+	audio.play();
+}
+document.querySelector(".i").addEventListener("click", handCli9);
+function handCli9() {
+    
+	var audio = new Audio("Music/EM9.wav");
+	audio.play();
+}
+document.querySelector(".j").addEventListener("click", handCli10);
+function handCli10() {
+    
+	var audio = new Audio("Music/EM10.wav");
+	audio.play();
+}
+document.querySelector(".k").addEventListener("click", handCli11);
+function handCli11() {
+    
+	var audio = new Audio("Music/EM11.wav");
+	audio.play();
+}
+document.querySelector(".l").addEventListener("click", handCli12);
+function handCli12() {
+    
+	var audio = new Audio("Music/EM12.wav");
+	audio.play();
+}
+document.querySelector(".m").addEventListener("click", handCli13);
+function handCli13() {
+    
+	var audio = new Audio("Music/EM13.wav");
+	audio.play();
+}
+document.querySelector(".n").addEventListener("click", handCli14);
+function handCli14() {
+    
+	var audio = new Audio("Music/EM14.wav");
+	audio.play();
+}
+document.querySelector(".o").addEventListener("click", handCli15);
+function handCli15() {
+    
+	var audio = new Audio("Music/EM15.wav");
+	audio.play();
+}
+document.querySelector(".p").addEventListener("click", handCli16);
+function handCli16() {
+    
+	var audio = new Audio("Music/EM16.wav");
+	audio.play();
+}	
+
+
